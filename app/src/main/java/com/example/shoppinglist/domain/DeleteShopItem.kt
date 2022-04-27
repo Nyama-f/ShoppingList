dba076709dafd0f8a39085e476064aafe1b9b931
Dmitry <mde.developer@gmail.com>
2022-04-27 12:54:18 +0500

    Building the Domain Layer architecture without implementation

---
+++ app/src/main/java/com/example/shoppinglist/domain/DeleteShopItem.kt
@@ -0,0 +1,7 @@
+package com.example.shoppinglist.domain
+
+class DeleteShopItem {
+    fun deleteShopitem(shopItem: ShopItem){
+
+    }
+}
---
+++ app/src/main/java/com/example/shoppinglist/domain/EditShopItem.kt
@@ -0,0 +1,7 @@
+package com.example.shoppinglist.domain
+
+class EditShopItem {
+    fun editShopItem(shopItem: ShopItem){
+
+    }
+}
